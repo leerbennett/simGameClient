2e30dcf6b8cfba9b57b16591d4270c4789261a2f
Lee Bennett <lee.bennett@sap.com>
2016-12-22 16:31:26 +0000

    Import from odata-boilerplate

---
+++ webapp/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"com/leerbennett/odata/controller/BaseController"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("com.leerbennett.odata.controller.App", {
+
+	});
+
+});
